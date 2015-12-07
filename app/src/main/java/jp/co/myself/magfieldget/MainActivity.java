3765a98c7e26e4dc0f46a3b7faaacb431368b8ab
xda-yoshioka <xda-yoshioka@kddilabs.jp>
2015-12-07 17:58:35 +0900

    センサで地磁気を取得して、画面(更新後)に表示します。

--- app/src/main/java/jp/co/myself/magfieldget/MainActivity.java
+++ app/src/main/java/jp/co/myself/magfieldget/MainActivity.java
@@ -1,16 +1,45 @@
 package jp.co.myself.magfieldget;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    // センサマネージャです。
+    private SensorManager mSensorManager;
+
+    // センサ(地磁気)です。
+    private Sensor mMagField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // センサマネージャのインスタンスを取得します。
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        // 地磁気センサを取得します。
+        mMagField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mMagField, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 
     @Override
@@ -33,5 +62,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        StringBuilder strBuild = new StringBuilder();
+
+        strBuild.append("X方向:" + event.values[0] + "μT\n");
+        strBuild.append("Y方向:" + event.values[1] + "μT\n");
+        strBuild.append("Z方向:" + event.values[2] + "μT\n");
+        TextView txt01 = (TextView) findViewById(R.id.txt01);
+        txt01.setText(strBuild.toString());
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 }
